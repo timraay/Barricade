14d6455a7468972bca1101884b721f1abd2fe464
Tim Raaymakers <43066279+timraay@users.noreply.github.com>
2024-02-25 01:22:04 +0100

    (fix) Event hooks

--- bunker/hooks.py
+++ bunker/hooks.py
@@ -1,43 +1,48 @@
 import asyncio
-from enum import Enum
+from collections import defaultdict
+from enum import Enum, auto
 from typing import Callable, Coroutine, Any
 
 from bunker import schemas
 from bunker.utils import log_task_error
 
-class EventHooks(Enum):
-    report_create: list[Callable[[schemas.ReportWithToken], Coroutine]] = list()
-    report_delete: list[Callable[[schemas.ReportWithToken], Coroutine]] = list()
-    player_ban: list[Callable[[schemas.Response], Coroutine]] = list()
-    player_unban: list[Callable[[schemas.Response], Coroutine]] = list()
+class EventHooks(str, Enum):
+    report_create = "report_create"
+    report_delete = "report_delete"
+    player_ban = "player_ban"
+    player_unban = "player_unban"
 
-    @staticmethod
-    def _invoke(hook_type: 'EventHooks', *args):
+    __hooks__: dict['EventHooks', list[Callable[[Any], Coroutine]]] = defaultdict(list)
+
+    def _invoke(self, *args):
         return [
             log_task_error(
                 task=asyncio.create_task(hook(*args)),
-                message=f"Failed to invoke {hook_type.name} hook {hook.__name__}"
-            ) for hook in hook_type.value
+                message=f"Failed to invoke {self.name} hook {hook.__name__}"
+            ) for hook in self.get()
         ]
+
+    def get(self):
+        return EventHooks.__hooks__[self]
+    
+    def register(self, func: Callable[[Any], Coroutine]):
+        self.get().append(func)
 
     @staticmethod
     def invoke_report_create(report: schemas.ReportWithToken):
-        return EventHooks._invoke(EventHooks.report_create, report)
+        return EventHooks.report_create._invoke(report)
 
     @staticmethod
     def invoke_report_delete(report: schemas.ReportWithToken):
-        return EventHooks._invoke(EventHooks.report_delete, report)
+        return EventHooks.report_delete._invoke(report)
 
     @staticmethod
     def invoke_player_ban(response: schemas.Response):
-        return EventHooks._invoke(EventHooks.player_ban, response)
+        return EventHooks.player_ban._invoke(response)
 
     @staticmethod
     def invoke_player_unban(response: schemas.Response):
-        return EventHooks._invoke(EventHooks.player_unban, response)
+        return EventHooks.player_unban._invoke(response)
 
 def add_hook(hook_type: EventHooks):
-    def _add_hook_inner(func: Callable[[Any], Coroutine]):
-        hook_type.value.append(func)
-        return func
-    return _add_hook_inner
+    return hook_type.register
