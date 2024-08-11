2456634fc7a5693047572210ed2143d8249f9155
Tim Raaymakers <43066279+timraay@users.noreply.github.com>
2024-08-11 17:12:06 +0200

    Remove unused constants

--- barricade/web/security.py
+++ barricade/web/security.py
@@ -19,11 +19,6 @@ from barricade.db import DatabaseDep, models
 from barricade.web import schemas as web_schemas
 from barricade.web.scopes import Scopes
 
-# to get a string like this run:
-# openssl rand -hex 32
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
