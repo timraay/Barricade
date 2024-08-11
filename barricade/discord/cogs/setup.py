839e8d0b33412207cdd58b7c3965da73f47b6254
Tim Raaymakers <43066279+timraay@users.noreply.github.com>
2024-08-11 17:07:49 +0200

    Design setup messages

--- barricade/discord/cogs/setup.py
+++ barricade/discord/cogs/setup.py
@@ -1,10 +1,10 @@
 import discord
 from discord import app_commands, Interaction
 from discord.ext import commands
-from barricade.constants import DISCORD_GUILD_ID
+from barricade.constants import DISCORD_GUILD_ID, DISCORD_OWNER_ROLE_ID, MAX_ADMIN_LIMIT
 
 from barricade.discord.bot import Bot
-from barricade.discord.utils import get_success_embed
+from barricade.discord.utils import get_command_mention, get_success_embed
 from barricade.discord.views.enroll import EnrollView
 from barricade.discord.views.submit_report import GetSubmissionURLView
 
@@ -17,6 +17,16 @@ class SetupCog(commands.GroupCog, group_name='setup'):
     @app_commands.command(name="send-submission-start-message")
     async def create_submission_start_message(self, interaction: Interaction):
         await interaction.channel.send(
+            content=(
+                "## Submitting a report"
+                "\nHad a player significantly disrupt your server? Then submit a report to Barricade!"
+                " That way, your evidence gets shared with other community admins, who can then"
+                " preemptively ban the player and prevent them from repeating their actions."
+                "\n\n"
+                "> Only severe violations should warrant getting someone banned across many community servers."
+                "\n> As a rule of thumb, **only report players that do not deserve a second chance**."
+                "\n_ _"
+            ),
             embed=discord.Embed(title="Submit a report"),
             view=GetSubmissionURLView()
         )
@@ -29,8 +39,24 @@ class SetupCog(commands.GroupCog, group_name='setup'):
     @app_commands.command(name="send-community-enroll-message")
     async def create_community_enroll_message(self, interaction: Interaction):
         await interaction.channel.send(
-            embed=discord.Embed(title="Request access to Barricade"),
-            view=EnrollView()
+            content=(
+                "### Are you the owner of a Hell Let Loose server?"
+                "\nRequest to join the Bunker to claim the"
+                f" <@&{DISCORD_OWNER_ROLE_ID}> role and get access to **server-related announcements**"
+                " as well as **Barricade**, the community's collaborative ban sharing platform."
+                "\n\n"
+                "> Do not submit more than one request per community."
+                f"\n> Once accepted, you can grant access to **{MAX_ADMIN_LIMIT}** additional admins"
+                f" using the {await get_command_mention(self.bot.tree, 'add-admin', guild_only=True)}"
+                " command."
+                "\n_ _"
+            ),
+            embed=discord.Embed(
+                title="Request access to Bunker",
+                description="-# Requests are manually reviewed. Please be patient."
+            ),
+            view=EnrollView(),
+            allowed_mentions=discord.AllowedMentions.none()
         )
 
         await interaction.response.send_message(
