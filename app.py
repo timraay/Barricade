0beae897eccd4d73e640283fcfaeeb13c2425853
Tim Raaymakers <43066279+timraay@users.noreply.github.com>
2024-08-20 23:54:16 +0200

    Log to file

--- app.py
+++ app.py
@@ -3,12 +3,16 @@
 import uvicorn
 import logging
 
-from barricade.constants import WEB_HOST, WEB_PORT
+from barricade.constants import WEB_HOST, WEB_PORT, LOGS_FOLDER
 from barricade.web.app import app
 
 logging.basicConfig(
     format="[%(asctime)s][%(levelname)7s][%(module)s.%(funcName)s:%(lineno)s] %(message)s",
-    level=logging.INFO
+    level=logging.INFO,
+    handlers=[
+        logging.FileHandler(filename=LOGS_FOLDER / "app.log", encoding="utf-8"),
+        logging.StreamHandler()
+    ]
 )
 
 if __name__ == '__main__':
