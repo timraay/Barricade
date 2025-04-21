fefbf788ab02342bf5c431e1ececf78f141575be
Tim Raaymakers <43066279+timraay@users.noreply.github.com>
2025-04-21 15:35:29 +0200

    Fix admins and roles not being updated on member join/leave

--- barricade/discord/cogs/admins.py
+++ barricade/discord/cogs/admins.py
@@ -1,7 +1,8 @@
+import logging
 from typing import TYPE_CHECKING
 
 import discord
-from discord import Interaction
+from discord import Interaction, RawMemberRemoveEvent
 from discord import app_commands
 from discord.ext import commands
 from discord.utils import escape_markdown as esc_md
@@ -18,6 +19,7 @@ from barricade.discord.views.admin_confirmation import (
     OwnershipTransferConfirmationView,
     LeaveCommunityConfirmationView
 )
+from barricade.logger import get_logger
 
 if TYPE_CHECKING:
     from barricade.discord.bot import Bot
@@ -28,69 +30,111 @@ class AdminsCog(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_join(self, member: discord.Member):
-        # Only run if user joins primary guild
-        if member.guild.id != DISCORD_GUILD_ID:
-            return
-        
-        async with session_factory() as db:
-            db_admin = await get_admin_by_id(db, discord_id=member.id)
-            # Return if member is not an admin of any community
-            if not db_admin or not db_admin.community:
+        logger = logging
+
+        try:
+            # Only run if user joins primary guild
+            if member.guild.id != DISCORD_GUILD_ID:
                 return
             
-            admin = schemas.Admin.model_validate(db_admin)
-            assert admin.community is not None
-            
-            # Grant roles
-            await update_user_roles(member.id, community=admin.community)
+            async with session_factory() as db:
+                db_admin = await get_admin_by_id(db, discord_id=member.id)
+                # Return if member is not an admin of any community
+                if not db_admin or not db_admin.community:
+                    return
+                
+                logger = get_logger(db_admin.community.id)
+                
+                admin = schemas.Admin.model_validate(db_admin)
+                assert admin.community is not None
+                
+                # Warning because in theory this shouldn't be possible
+                logger.warning(
+                    "Admin %s (%s) rejoined primary guild, granting them their roles",
+                    db_admin.name, db_admin.discord_id
+                )
+
+                # Grant roles
+                await update_user_roles(member.id, community=admin.community)
+        except:
+            # discord.py would have silently eaten the exception
+            logger.exception("Failed to invoke \"on_member_join\" event")
 
     @commands.Cog.listener()
-    async def on_member_leave(self, member: discord.Member):
-        # Only run if user joins primary guild
-        if member.guild.id != DISCORD_GUILD_ID:
-            return
-        
-        async with session_factory() as db:
-            db_admin = await get_admin_by_id(db, discord_id=member.id)
-            # Return if member is not an admin of any community
-            if not db_admin or not db_admin.community:
+    async def on_raw_member_remove(self, payload: RawMemberRemoveEvent):
+        logger = logging
+
+        try:
+            # Only run if user joins primary guild
+            if payload.guild_id != DISCORD_GUILD_ID:
                 return
-            
-            db_community = db_admin.community
-            
-            if db_admin.owned_community:
-                # The user is the community owner
-                await db_community.awaitable_attrs.admins
-                if len(db_community.admins) > 1:
-                    # If there's other admins in the community, transfer ownership to
-                    # an arbitrary one and then remove them
-                    db_new_owner = next(
-                        admin
-                        for admin in db_community.admins
-                        if admin.discord_id != member.id
-                    )
-                    await transfer_ownership(
-                        db, db_community.id, db_new_owner.discord_id,
-                        by=member, # type: ignore
-                    )
-                    await admin_leave_community(
-                        db, db_admin,
-                        by=member, # type: ignore
-                    )
+
+            async with session_factory.begin() as db:
+                db_admin = await get_admin_by_id(db, discord_id=payload.user.id)
+                # Return if member is not an admin of any community
+                if not db_admin or not db_admin.community:
+                    return
+                
+                db_community = db_admin.community
+                community = schemas.CommunityRef.model_validate(db_community)
+
+                logger = get_logger(community.id)
+                
+                if db_admin.owned_community:
+                    # The user is the community owner
+                    await db_community.awaitable_attrs.admins
+                    if len(db_community.admins) > 1:
+                        # If there's other admins in the community, transfer ownership to
+                        # an arbitrary one and then remove them
+                        db_new_owner = next(
+                            admin
+                            for admin in db_community.admins
+                            if admin.discord_id != payload.user.id
+                        )
+                        new_owner = schemas.AdminRef.model_validate(db_new_owner)
+
+                        logger.info(
+                            "Owner %s (%s) left primary guild, transferring ownership to %s (%s)",
+                            db_admin.name, db_admin.discord_id, new_owner.name, new_owner.discord_id
+                        )
+
+                        db.expunge(db_community)
+                        db.expunge(db_new_owner)
+                        await transfer_ownership(
+                            db, community.id, new_owner.discord_id,
+                            by=payload.user, # type: ignore
+                        )
+
+                        await db.refresh(db_admin)
+                        await admin_leave_community(
+                            db, db_admin,
+                            by=payload.user, # type: ignore
+                        )
+
+                    else:
+                        # If no admins remain, abandon the community instead
+                        logger.info(
+                            "Owner %s (%s) left primary guild, community was abandoned",
+                            db_admin.name, db_admin.discord_id
+                        )
+                        await abandon_community(
+                            db, community.id,
+                            by=payload.user, # type: ignore
+                        )
 
                 else:
-                    # If no admins remain, abandon the community instead
-                    await abandon_community(
-                        db, db_community.id,
-                        by=member, # type: ignore
+                    logger.info(
+                        "Admin %s (%s) left primary guild, revoking their admin priviliges",
+                        db_admin.name, db_admin.discord_id
                     )
-
-            else:
-                # If the user is just an admin, simply remove them
-                await admin_leave_community(
-                    db, db_admin,
-                    by=member, # type: ignore
-                )
+                    # If the user is just an admin, simply remove them
+                    await admin_leave_community(
+                        db, db_admin,
+                        by=payload.user, # type: ignore
+                    )
+        except:
+            # discord.py would have silently eaten the exception
+            logger.exception("Failed to invoke \"on_raw_member_remove\" event")
     
     @app_commands.command(name="add-admin", description="Add one of your community's admins to the Bunker")
     @app_commands.guilds(DISCORD_GUILD_ID)
