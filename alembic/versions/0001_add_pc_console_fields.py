53ba2b6d84908c435f3991613113a7588b375f5d
Tim Raaymakers <43066279+timraay@users.noreply.github.com>
2026-03-11 14:47:28 +0000

    Add initial migration

--- alembic/versions/0001_add_pc_console_fields.py
+++ alembic/versions/0001_add_pc_console_fields.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '80ff93d59a99'
-down_revision: Union[str, None] = None
+down_revision: Union[str, None] = '2282f1b2a88c'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
